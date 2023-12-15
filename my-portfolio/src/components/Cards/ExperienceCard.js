9e773d87a21caefe7e17e0b7a60b316d2e2ec37e
Francesco Divincenzo <94543040+DiviFra@users.noreply.github.com>
2023-12-15 11:42:48 +0100

    upload modules

---
+++ my-portfolio/src/components/Cards/ExperienceCard.js
@@ -0,0 +1,141 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Description = styled.div`
+    width: 100%;
+    font-size: 15px;
+    font-weight: 400;
+    color: ${({ theme }) => theme.text_primary + 99};
+    margin-bottom: 10px;
+    @media only screen and (max-width: 768px){
+        font-size: 12px;
+    }
+`
+
+const companyStyle = {
+    color: "purple"
+};
+
+const dateStyle = {
+    color: "violet"
+};
+
+
+const Span = styled.span`
+overflow: hidden;
+display: -webkit-box;
+max-width: 100%;
+-webkit-line-clamp: 4;
+-webkit-box-orient: vertical;
+text-overflow: ellipsis;
+`
+
+const Card = styled.div`
+    width: 650px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+//  background: linear-gradient(to left, rgba(128,0,128,0.3), rgba(255,255,255,0.0));
+    padding: 12px 16px;
+    justify-content: space-between;
+    position: relative;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    transition: all 0.3s ease-in-out;
+    &:hover{
+        box-shadow: 0px 0px 20px rgba(0,0,0,0.2);
+        transform: translateY(-5px);
+    }
+    @media only screen and (max-width: 768px){
+        padding: 10px;
+        gap: 8px;
+        width: 300px;
+    }
+
+    &:hover ${Span}{
+        overflow: visible;
+        -webkit-line-clamp: unset;
+
+    }
+
+    border: 0.1px solid #306EE8;
+    box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
+`
+
+const Top = styled.div`
+    width: 100%;
+    display: flex;
+    gap: 12px
+`
+
+
+const Body = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column; 
+`
+
+
+const Role = styled.div`
+    font-size: 18px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.text_primary + 99};
+    @media only screen and (max-width: 768px){
+        font-size: 14px;
+    }
+`
+
+const Company = styled.div`
+    font-size: 20px;
+    font-weight: 500;
+    color: white;
+    @media only screen and (max-width: 768px){
+        font-size: 12px;
+    }
+`
+
+const Date = styled.div`
+    font-size: 12px;
+    font-weight: 400;
+    color: ${({ theme }) => theme.text_secondary + 80};
+    @media only screen and (max-width: 768px){
+        font-size: 10px;
+    }
+`
+
+
+const Skill = styled.div`
+    font-size: 14px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.text_primary + 99};
+    @media only screen and (max-width: 768px){
+        font-size: 14px;
+    }
+`
+
+
+
+
+
+const ExperienceCard = ({experience }) => {
+
+    const formattedSkills = experience.skills.join(' ');
+
+    return <Card>
+        <Top> 
+            <Body>
+                <Role>{experience.role}</Role> 
+                <Company>{experience.company}</Company>
+                <Date>{experience.date}</Date>
+            </Body>
+        </Top>
+        <Description></Description>
+        <Skill> Skills: { }
+            {formattedSkills}
+        </Skill>
+    </Card>;
+};
+
+export default ExperienceCard;
---
+++ my-portfolio/src/data/constants.js
@@ -0,0 +1,203 @@
+
+export const Bio = {
+    name: "Divincenzo Francesco",
+    roles: [
+      "Full Stack Developer",
+      "Back-End Developer",
+      "Front-End Developer",
+      "Developer",
+    ],
+    description:
+      "Description of me",
+    github: "https://github.com/DiviFra",
+    resume:
+      "https://firebasestorage.googleapis.com/v0/b/divincenzo-portfolio.appspot.com/o/CV%20Francesco%20Divincenzo.pdf?alt=media&token=783edd67-e2ad-43af-817f-50d34f629409",
+    linkedin: "https://www.linkedin.com/in/francesco-divincenzo-a09a42172/",
+    insta: "https://www.instagram.com/divart7/",
+    facebook: "https://www.facebook.com/watashiwaerudesu",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "Postgresql",
+          image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Docker",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+        },
+        {
+          name: "Adobe PS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/a/af/Adobe_Photoshop_CC_icon.svg",
+        },
+        {
+          name: "Adobe Ai",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/f/fb/Adobe_Illustrator_CC_icon.svg",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.microitalia.it%2Fcategory%2Fsenza-categoria%2Fpage%2F2%2F&psig=AOvVaw1ZImd7NGOHr5lLe-3XysCa&ust=1700217245585000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPCRo_moyIIDFQAAAAAdAAAAABAI",
+      role: "WordPress Admin",
+      company: "M.I.Cro.Italia OdV",
+      date: "Set 2018 - June 2021",
+      desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+      skills: [
+        "WordPRess",
+        "MySQL",
+      ],
+    },
+    {
+      id: 0,
+      role: "Web Graphic, Grapich Designer",
+      company: "M.I.Cro.Italia OdV",
+      date: "Set 2018 - June 2021",
+      desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+      skills: [
+        "Adobe Photoshop ",
+        "Adobe Illustrator",
+      ],
+    },
+    {
+      id: 1,
+      img: "",
+      role: "Front-End",
+      company: "smc Treviso",
+      date: "June 2022 - June 2024",
+      desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+      skills: [
+        "ReactJS",
+        "NodeJs",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Docker",
+        "MongoDB",
+        "Visual Studio Code",
+      ],
+    },
+    {
+      id: 1,
+      role: "Back-End",
+      company: "smc Treviso",
+      date: "June 2022 - June 2024",
+      desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+      skills: [
+        "Java",
+        "Java EE",
+        "HTML",
+        "MySQL",
+        "Docker",
+        "Postman",
+        "IntelliJ",
+        "Eclipse",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img:"https://firebasestorage.googleapis.com/v0/b/divincenzo-portfolio.appspot.com/o/image%2FMIUR.jpg?alt=media&token=07511f14-483e-4f9d-87e0-734bd949b06e",
+      school: "Liceo Scientifico G. Tarantino",
+      date: "Set 2009 - jul 2014",
+    },
+    {
+      id: 1,
+      img:"https://firebasestorage.googleapis.com/v0/b/divincenzo-portfolio.appspot.com/o/image%2Fattestato2.jpg?alt=media&token=cbb8443a-6736-4c5b-9805-dd8049afc696",
+      school: "ATENA SPA",
+      certificate: "Sviluppatore Java"
+,     date: "Jan 2022 - Jul 2022",
+    },
+    {
+      id: 2,
+      img:"https://firebasestorage.googleapis.com/v0/b/divincenzo-portfolio.appspot.com/o/image%2Fattestato1.jpg?alt=media&token=c5057530-8d21-43da-8fc0-f8e4ce8db41a",
+      school: "RANDSTAD ITALIA SPA",
+      certificate: "Accademy Java FullStack",
+      date: "Apr 2022 - Jun 2022",
+    },
+  ];
+  
